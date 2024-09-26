5c7453d4fd0dd086e0feb1dcfd09dc4443b4f4de
shelenydmity <shelenydmitry@gmail.com>
2024-09-26 13:14:55 +0300

    added better table representation

--- bd_lab_1/bd_lab_1.cpp
+++ bd_lab_1/bd_lab_1.cpp
@@ -9,6 +9,7 @@ MYSQL_ROW row;
 std::string query;
 int num_fields;
 int num_rows;
+int* LensFields = nullptr;
 
 int main()
 {
@@ -35,24 +36,58 @@ int main()
 					std::getline(std::cin, query);
 					continue;
 				}
-				//вывод шапочки (Hr) - имен атрибутов!
+
+				//получим максимальные длины строк каждого атрибута, чтобы вывести таблицу красиво - создадим массив с длинами для каждого столбца
+				//LensFields[i] - это максимальная длина строки i-го атрибута
+
 				num_fields = mysql_num_fields(res);
+				int* LensFields = new int[num_fields];
 				for (int i = 0; i < num_fields; i++) {
 					field = mysql_fetch_field(res);
-					std::cout << field->name << "\t| ";
+					LensFields[i] = strlen((field->name));
 				}
-				std::cout << std::endl;
-
-				//вывод тела (Br) - кортежей!
 				num_rows = mysql_num_rows(res);
 				for (int j = 0; j < num_rows; j++) {
 					row = mysql_fetch_row(res);
 					for (int i = 0; i < num_fields; i++) {
-						std::cout << row[i] << "\t| ";
+						if (strlen(row[i]) > LensFields[i]) {
+							LensFields[i] = strlen(row[i]);
+						}
+					}
+				}
+				
+				res = nullptr;
+				field = nullptr;
+				row = nullptr;
+				mysql_query(conn, query.c_str());
+				res = mysql_store_result(conn);
+
+				//вывод шапочки (Hr) - имен атрибутов!
+				
+				for (int i = 0; i < num_fields; i++) {
+					field = mysql_fetch_field(res);
+					std::cout << field->name << " ";
+					for (int j = 0; j < LensFields[i] - strlen(field->name); j++) {
+						std::cout << " ";
+					}
+					std::cout << "|";
+				}
+				std::cout << std::endl;
+
+				//вывод тела (Br) - кортежей!
+				for (int k = 0; k < num_rows; k++) {
+					row = mysql_fetch_row(res);
+					for (int i = 0; i < num_fields; i++) {
+						std::cout << row[i] << " ";
+						for (int j = 0; j < LensFields[i] - strlen(row[i]); j++) {
+							std::cout << " ";
+						}
+						std::cout << "|";
 					}
 					std::cout << std::endl;
 				}
 			}
+			std::cout << "enter query:" << std::endl;
 			std::getline(std::cin, query);
 		}
 		mysql_close(conn);
@@ -62,4 +97,5 @@ int main()
 		mysql_close(conn);
 		return 1;
 	}
+	delete[] LensFields;
 }
